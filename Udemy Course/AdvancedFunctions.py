a0d0891f5ac3bd1f7dfbb6a953bd8e78ea34f49f
Shaun Clark <shaun.m.clark@gmail.com>
2025-04-27 11:08:42 +0200

    Update AdvancedFunctions.py

--- Udemy Course/AdvancedFunctions.py
+++ Udemy Course/AdvancedFunctions.py
@@ -1,3 +1,4 @@
+# Advnced Numbers
 hexnum = hex(1024)
 print(f"Hexadecimal: {hexnum}")
 binnum = bin(1024)
@@ -6,6 +7,7 @@ print(pow(2,4))    #Same as 2**4
 print(abs(-3))     #Absolute number
 print(round(3.141529, 2))
 
+# Advanced Sets
 set1 = {1,2,3,4}
 print(set1)
 set2 = set1.copy()
@@ -13,5 +15,6 @@ set2.add(5)
 print(f"Difference: {set2.difference(set1)}")       #Shows elements that are different
 print(f"Intersection: {set1.intersection(set2)}")   #Shows common elements
 
+# Advanced Dictionaries
 dict1 = {"k1": 1, "k2": 2, "k3":3}
 print(dict1.items())
